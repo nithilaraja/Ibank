13fbd3ae3f096a92aa4b5cf0d36749fb5ee435a1
nithilaraja <nithilaraja07@gmail.com>
2022-04-29 10:09:44 -0500

    third commit

--- src/test/java/com/ibank/Testcases/TC_LoginDDT_002.java
+++ src/test/java/com/ibank/Testcases/TC_LoginDDT_002.java
@@ -41,7 +41,7 @@ public class TC_LoginDDT_002 extends BaseClass
 			Thread.sleep(2000);
 			driver.switchTo().alert().accept();//logout alert
 			driver.switchTo().defaultContent();
-			log.info("logged out successfully");
+			log.info("Success logged out successfully");
 		}
 	}
 	public boolean isAlert()
